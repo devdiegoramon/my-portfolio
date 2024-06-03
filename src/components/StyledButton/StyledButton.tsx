08926a5f37ea2bf8bd7bb1020aaf902623e652fb
Diego Ramon <brilhocelestebc15@gmail.com>
2024-06-03 00:42:33 -0300

    REFACTOR: Correção Binding element children implicitly has an any

--- src/components/StyledButton/StyledButton.tsx
+++ src/components/StyledButton/StyledButton.tsx
@@ -1,27 +1,30 @@
 import { styled } from "@mui/material";
+import { ReactNode } from "react";
 
-const StyledButton = ({ children }) => {
-  const StyledButton = styled("button")(({ theme }) => ({
-    backgroundColor: "transparent",
-    border: "1px solid ${theme.palette.primary.contrastText}",
-    borderRadius: "3px",
-    padding: "5px 15px",
-    width: "100%",
-    color: theme.palette.primary.contrastText,
-    display: "inline-flex",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: "5px",
-    "&:hover": {
-      backgroundColor: theme.palette.secondary.light,
-    },
-  }));
+// Definindo a interface para os props
+interface StyledButtonProps {
+  children: ReactNode;
+}
 
-  return (
-    <>
-      <StyledButton>{children}</StyledButton>
-    </>
-  );
+// Definindo o botão estilizado fora do componente para evitar redefinição de variável
+const Button = styled("button")(({ theme }) => ({
+  backgroundColor: "transparent",
+  border: `1px solid ${theme.palette.primary.contrastText}`,
+  borderRadius: "3px",
+  padding: "5px 15px",
+  width: "100%",
+  color: theme.palette.primary.contrastText,
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: "5px",
+  "&:hover": {
+    backgroundColor: theme.palette.secondary.light,
+  },
+}));
+
+const StyledButton: React.FC<StyledButtonProps> = ({ children }) => {
+  return <Button>{children}</Button>;
 };
 
 export default StyledButton;

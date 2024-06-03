d98c15f81cf5cebfc556d29e53e567d70235476e
Diego Ramon <brilhocelestebc15@gmail.com>
2024-06-02 22:06:14 -0300

    FEAT: Adicionando botões Github e Linkedin a HeroPage

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,5 +1,9 @@
-import { Container, Grid, Typography, styled } from "@mui/material";
+import { Button, Container, Grid, Typography, styled } from "@mui/material";
 import Avatar from "../../../../assets/images/pessoas/eu/minhafoto-min.png";
+
+// ICONES
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Hero = () => {
   const StyledHero = styled("div")(() => ({
@@ -8,7 +12,7 @@ const Hero = () => {
   }))
 
   const StyledImg = styled("img")(() => ({
-    width: "30%",
+    width: "100%",
     borderRadius: "50%",
   }));
 
@@ -20,13 +24,19 @@ const Hero = () => {
 
           <Grid container spacing={2}>
 
-            <Grid item xs={4}>
+            <Grid item xs={12} md={4}>
               <StyledImg src={Avatar} />
             </Grid>
 
-            <Grid item xs={8}>
-              <Typography color='primary' variant='h1'>Diego Ramon</Typography>
-              <Typography color='primary' variant='h2'>I'm Software Engineer</Typography>
+            <Grid item xs={12} md={8}>
+              <Typography color='primary' variant='h1' textAlign='center'>Diego Ramon</Typography>
+              <Typography color='primary' variant='h2' textAlign='center'>I'm Software Engineer</Typography>
+              <Button>
+              <GitHubIcon/>
+                 GitHub</Button>
+              <Button>
+                <LinkedInIcon/> 
+                    Linkedin</Button>
             </Grid>
 
           </Grid>
